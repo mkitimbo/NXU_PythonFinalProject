735424dd5ebd878660ff64abfc1ec3f1ec7e0b8c
mkitimbo <kitimbo@gmail.com>
2025-04-20 15:00:30 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,17 +6,15 @@ mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
-    
- 
-    
     # MongoDB Atlas connection string
     #app.config["MONGO_URI"] = "mongodb+srv://mkitimbo:txaHdneje2CZmkOt@kmt.tslnr0v.mongodb.net/income_db?retryWrites=true&w=majority&tls=true"
-    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    #app.config["MONGO_URI"] = os.getenv("MONGO_URI")
    # mongo_uri = os.environ.get("MONGO_URI", "mongodb+srv://mkitimbo:txaHdneje2CZmkOt@kmt.tslnr0v.mongodb.net/income_db?retryWrites=true&w=majority&tls=true")
     
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
     mongo.init_app(app)
 
-   # Import blueprints here to avoid circular imports
+   #Import blueprints here to avoid circular imports
     from app.routes import main
     app.register_blueprint(main)
     
