106bf5286cd48dd965558def3523be7f8b3561e6
mkitimbo <kitimbo@gmail.com>
2025-04-20 18:31:23 +0200

    reactivate url service on renders

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 # run.py
 import os
+# app.py or main.py
+import requests
+
+def ping_self():
+    try:
+        requests.get("https://pythonfinalproj.onrender.com/")
+        print("Self-ping successful")
+    except Exception as e:
+        print(f"Ping failed: {e}")
+
+# Ping on startup
+ping_self()
+
 from app import create_app
 
 app = create_app()
