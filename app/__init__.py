22f22159db733ca64f65f4001a077bcedeb98788
mkitimbo <kitimbo@gmail.com>
2025-04-20 15:57:07 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,18 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 
-# Load environment variables from .env if available
-load_dotenv()
-
 mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
-
-    mongo_uri = os.getenv("MONGO_URI")
-    if not mongo_uri:
-        raise ValueError("❌ MONGO_URI is not set. Please check your environment variables.")
-
-    # Apply config and init Mongo
-    app.config["MONGO_URI"] = mongo_uri
+    
+    # MongoDB Atlas connection string
+    app.config["MONGO_URI"] = "mongodb+srv://mkitimbo:txaHdneje2CZmkOt@kmt.tslnr0v.mongodb.net/income_db?retryWrites=true&w=majority&tls=true"
+    
     mongo.init_app(app)
 
-    # Register blueprints
+   # Import blueprints here to avoid circular imports
     from app.routes import main
     app.register_blueprint(main)
-
+    
     return app
